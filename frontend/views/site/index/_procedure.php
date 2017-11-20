42e79309679a8ff2aa91f3320e96156512133bc5
gongkunjxl@163.com <gongkunjxl@163.com>
2017-11-21 00:11:11 +0800

    logo

--- frontend/views/site/index/_procedure.php
+++ frontend/views/site/index/_procedure.php
@@ -7,7 +7,7 @@ use  yii\helpers\Url;
 <div class="container">
 <h1 style="text-align: center; font-weight: 300; font-size: 4rem; line-height: 4rem">已有<strong style="font-weight: 400;color: red">936827</strong>人让专家解开心结</h1>  
 <div style="width: 100%; height: 10px;"></div>
-<h3 style="text-align: center;font-weight: 400; margin-top: 2rem;">联系客服 立刻解决你的困扰</h3>
+<h3 style="text-align: center;font-weight: 400; margin-top: 2rem;">联系客服 立刻解决您的困扰</h3>
 <!-- <p style="text-align: center;">不满意 可 100% 退款</p>
 <img style="width: 100%; " src="images/procedure.png"> -->
 
